d1781269bbf99f4a22579d8c2c68da4c9f350f6b
iLexN <ilex.job@gmail.com>
2016-05-25 16:27:16 +0800

    config example update

--- config/config.example.php
+++ config/config.example.php
@@ -8,9 +8,18 @@ return array(
     ),
     //backup email
     'globalemails'=> "",
+    'globalemailsFrom'=> "",
     'p4email'=>array(
         'PP' => array( 'mailBody'=> 'pp_email_client.html' , 'mailFromName'=>'PacificPrime','mailFromAc'=>''),
         'PPI' => array( 'mailBody'=> 'pp_email_client.html' , 'mailFromName'=>'PacificPrime','mailFromAc'=>''),
         'UMI' => array( 'mailBody'=> 'umi_email_client.html' , 'mailFromName'=>'UMI','mailFromAc'=>''),
+    ),
+    'logConfig'=>array(
+            'name' => 'app',
+            'path' => __DIR__ . '/../logs/'.date("Y-m-d").'.log',
+    ),
+    'twigConfig'=>array(
+            'templatePath' => __DIR__ . '/../template/',
+            'cachePath' => __DIR__ . '/../cache/',
     ),
 );
